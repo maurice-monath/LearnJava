68abff93da5cdabaf79f2e1ca20253050699d585
Maurice Monath <maurice@monath.xyz>
2023-10-19 13:44:39 +0200

    DO Task Nr.1 MyFirstClass

--- src/main/java/myfirstclass/FirstClass.java
+++ src/main/java/myfirstclass/FirstClass.java
@@ -1,5 +1,16 @@
 package myfirstclass;
 
+/**
+ * This is my first class in Java.
+ * It prints "Hello World!" to the console.
+ */
 public class FirstClass {
-  
+
+  /**
+   * This is my first class in Java.
+   * It prints "Hello World!" to the console.
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
 }
