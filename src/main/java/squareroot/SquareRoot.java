14e16dc32119618de6dc675003effe225b99c4cf
Maurice Monath <maurice@monath.xyz>
2023-11-23 10:32:30 +0100

    Correct SquareRoot

--- src/main/java/squareroot/SquareRoot.java
+++ src/main/java/squareroot/SquareRoot.java
@@ -1,19 +1,25 @@
 package squareroot;
 
+/**
+ * ture oder false berrechung ob bases die wurzel aus nummer ist.
+ */
 public class SquareRoot {
 
+  /**
+   * @param base   Wurzel aus nummer.
+   * @param number Nummer
+   * @return ture oder false
+   */
   public static boolean isSquareRoot(int base, long number) {
 
-    boolean result = (base * base) == number
-                     ? true
-                     : false;
-
-    return result;
+    return ((long) base * base) == number;
   }
 
+  /**
+   * @param args not used
+   */
   public static void main(String[] args) {
-    int base = 5;
-    long number = 25;
-    System.out.println(isSquareRoot(base, number));
+    
+    System.out.println(isSquareRoot(5, 25));
   }
 }
