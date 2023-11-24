af0990aeae54762912b4279206e90738bde248c9
Maurice Monath <maurice@monath.xyz>
2023-11-24 08:36:12 +0100

    Correct SquareRoot
    Muss noch überprüft werden da bei -5 und 25 true rauskommt da -5*-5 theoretisch +25 sind aber -5 ist nicht die wurzel aus 25

--- src/main/java/squareroot/SquareRoot.java
+++ src/main/java/squareroot/SquareRoot.java
@@ -1,25 +1,27 @@
 package squareroot;
 
 /**
- * ture oder false berrechung ob bases die wurzel aus nummer ist.
+ * Überprüft, ob die gegebene Basis die Quadratwurzel der angegebenen Zahl ist.
  */
 public class SquareRoot {
 
   /**
-   * @param base   Wurzel aus nummer.
+   * Überprüft, ob die Quadratzahl der Basis gleich der gegebenen Zahl ist.
+   *
+   * @param base   Wurzel aus der Nummer.
    * @param number Nummer
-   * @return ture oder false
+   * @return true, wenn die Basis die Quadratwurzel der Nummer ist, sonst false.
    */
   public static boolean isSquareRoot(int base, long number) {
-
-    return ((long) base * base) == number;
+    return (long) base * base == number;
   }
 
   /**
-   * @param args not used
+   * Hauptmethode zum Testen der isSquareRoot-Methode.
+   *
+   * @param args nicht verwendet
    */
   public static void main(String[] args) {
-    
-    System.out.println(isSquareRoot(5, 25));
+    System.out.println(isSquareRoot(-5, 25));
   }
 }
